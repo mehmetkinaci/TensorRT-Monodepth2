faff0053651812af6f1fa890f0ab6e7936e08d32
Mehmet Kınacı <60465356+mehmetkinaci@users.noreply.github.com>
2022-04-15 16:18:50 +0300

    Add files via upload

---
+++ build/CMakeFiles/experimental.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/experimental.dir/main.cpp.o"
+  "experimental.pdb"
+  "experimental"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/experimental.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
